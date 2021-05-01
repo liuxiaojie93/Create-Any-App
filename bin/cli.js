6edd4c497d7f4fc214620269c2fbcff206f9c9e1
liuxiaojie <1245145809@qq.com>
2021-05-01 11:48:21 +0800

    模块拆分

--- bin/cli.js
+++ bin/cli.js
@@ -1,21 +1,17 @@
 #!/usr/bin/env node
 const { Command } = require("commander");
-const fs = require("fs");
 const path = require("path");
 const program = new Command();
 const chalk = require("chalk");
-const { validateProjectName } = require("./utils/validateProjectName");
-const download = require('download-git-repo');
-
+const { validateProjectName } = require("../lib/validateProjectName");
+const create = require("../lib/create")
 program
-  .command("ck <projectName>")
+  .command("create <projectName>")
   .alias("ck")
-  .option("-r, --recursive", "Remove recursively")
-  .option("-d --drink [drink]", "Drink", "Beer")
+  .option("-f, --force", "rm folder force")
   .action(async function (projectName, cmd) {
     // TODO 校验projectName
-    const cwd = process.cwd();
-    const targetDir = path.resolve(cwd, projectName || ".");
+    
     const result = validateProjectName(projectName);
     if (!result.validForNewPackages) {
       console.error(chalk.red(`Invalid project name: "${name}"`));
@@ -29,24 +25,7 @@ program
         });
       exit(1);
     }
-
-    // if (fs.existsSync(targetDir) && !options.merge) {
-    //   if (options.force) {
-    //     await fs.remove(targetDir);
-    //   } else {
-    //     fs.mkdirSync(`${process.cwd()}/${projectName}`);
-    //   }
-    // } else {
-    //   fs.mkdirSync(`${process.cwd()}/${projectName}`);
-    // }
-    // if (fs.existsSync(targetDir)){
-    //     fs.rmdirSync(targetDir);
-    // }
-    fs.mkdirSync(`${process.cwd()}/${projectName}`);
-    console.log();
-    download("github:liuxiaojie93/react-demo",targetDir,function(err){
-       console.log(err?err:chalk.blue("create success!"));
-    })
+    create(projectName,cmd)
   });
 
   program.version("0.0.1", "-v,--version").parse(process.argv);
